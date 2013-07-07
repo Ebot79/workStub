394897becf82456e72426197bf10645e85ae8d99
Ebot79 <eash79@gmail.com>
2013-07-07 15:19:29 -0400

    code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkStub::Application.config.secret_token = 'c036deb76f256533f729698d09506b99df2e28cb83d0efe4a87b0530c317fb6749f346dedf93cc73f2cceaa19b2dea710e6689741d15b2ff4fd1d43e7520e90a'
